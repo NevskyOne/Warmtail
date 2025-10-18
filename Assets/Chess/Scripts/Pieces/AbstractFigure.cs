97871dfb17c1c7850a26d21c6642dad035fc71ae
Timm15 <zoidze.laz2018@gmail.com>
2025-10-18 19:20:44 +0300

    1.2

--- Assets/Chess/Scripts/Pieces/AbstractFigure.cs
+++ Assets/Chess/Scripts/Pieces/AbstractFigure.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class BasePiece : MonoBehaviour
+public abstract class BasePiece : EventTrigger
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [HideInInspector] public Color mColor = Color.clear;
+    protected Cell m0riginalCell = null;
+    protected Cell mCurrentCell = null;
+    protected RectTransform mRectTransform = null;
+    protected PieceManager mPieceManager;
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
     {
-        
+    mPieceManager = newPieceManager;
+    mColor = newTeamColor;
+    GetComponent<Image>().color = newSpriteColor;
+    mRectTransform = GetComponent<RectTransform>();
+    }
+    
+    public void Place(Cell newCell)
+    {
+
+        mCurrentCell = newCell;
+        mOriginalCell = newCell;
+        mCurrentCell.mCurrentPiece = this;
+
+        transform.position = newCell.transform.position;
+        game0bject.SetActive(true);
     }
 }
