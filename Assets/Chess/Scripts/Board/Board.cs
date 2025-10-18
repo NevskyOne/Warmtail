74aa0fcae7eac5456456b8c0892ec727f6c6d0a4
PolikGnomik <pchesnokova09@gmail.com>
2025-10-18 19:43:13 +0300

    1.4

--- Assets/Chess/Scripts/Board/Board.cs
+++ Assets/Chess/Scripts/Board/Board.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
-    public GameObject CellPrefab;
+    public GameObject mCellPrefab;
 
-    public Cell[,] AllCells = new Cell[8 , 8];
+    public Cell[,] mAllCells = new Cell[8 , 8];
 
     public void Create()
     {
@@ -14,11 +14,23 @@ public class Board : MonoBehaviour
         {
             for (int x = 0; x < 8; x++)
             {
-                GameObject newCell = Instantiate(CellPrefab, transform);
+                GameObject newCell = Instantiate(mCellPrefab, transform);
                 RectTransform rectTrans = newCell.GetComponent<RectTransform>();
 
-                AllCells[x, y] = newCell.GetComponent<Cell>();
-                AllCells[x, y].Setup(new Vector2Int(x, y), this);
+                mAllCells[x, y] = newCell.GetComponent<Cell>();
+                mAllCells[x, y].Setup(new Vector2Int(x, y), this);
+            }
+
+        }
+
+        for (int x = 0; x < 8; x+=2)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                int offset = (y % 2 != 0) ?  0 : 1;
+                int finalX = x + offset;
+
+                mAllCells[finalX, y].GetComponent<Image>().color = new Color32(17, 96, 98, 255);
             }
         }
     }
