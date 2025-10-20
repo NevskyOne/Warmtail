e3bf9fd4601039bd4ce9f71ab197ae16c6d31301
Timm15 <zoidze.laz2018@gmail.com>
2025-10-20 21:47:02 +0300

    131231231

--- Assets/Chess/Scripts/Pieces/Pawn.cs
+++ Assets/Chess/Scripts/Pieces/Pawn.cs
@@ -4,24 +4,14 @@ using UnityEngine.UI;
 
 public class Pawn : BasePiece
 {
-    //private bool mIsFirstMove = true;
-    //public override void Setup(Color newTeamColor, Color newSpriteColor, PieceManager newPieceManager)
-    //{
-    //    base.Setup(newTeamColor, newSpriteColor, newPieceManager);
 
-    //    mMovement = mColor == Color.white ? new Vector3Int(0, 1, 1) : new Vector3Int(0, -1, -1);
-    //    GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Pawn");
-    //}
-    //protected override void Move()
-    //{
-    //    base.Move();
-    //    mIsFirstMove = false;
-    //    CheckForPromotion();
-    //}
-    //public override void Reset()
-    //{
-    //    base.Reset();
-    //    mIsFirstMove = true;
-    //}
+    public override void Setup(Color newTeamColor, Color newSpriteColor, PieceManager newPieceManager)
+    {
+        base.Setup(newTeamColor, newSpriteColor, newPieceManager);
+
+        mMovement = mColor == Color.white ? new Vector3Int(0, 1, 1) : new Vector3Int(0, -1, -1);
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Pawn");
+    }
+   
 
 }
--- Assets/Chess/Scripts/Pieces/Rook.cs
+++ Assets/Chess/Scripts/Pieces/Rook.cs
@@ -3,11 +3,11 @@ using UnityEngine.UI;
 
 public class Rook : BasePiece
 {
-    //public override void Setup(Color newTeamColor, Color newSpriteColor, PieceManager newPieceManager)
-    //{
-    //    base.Setup(newTeamColor, newSpriteColor, newPieceManager);
+    public override void Setup(Color newTeamColor, Color newSpriteColor, PieceManager newPieceManager)
+    {
+        base.Setup(newTeamColor, newSpriteColor, newPieceManager);
 
-    //    mMovement = new Vector3Int(7, 7, 0);
-    //    GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Rook");
-    //}
+        mMovement = new Vector3Int(7, 7, 0);
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Rook");
+    }
 }
